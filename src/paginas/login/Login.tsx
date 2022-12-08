73ff5c8f0bb32eb5385613f86cf825d02c43b5ff
gabrielsjobs <112710128+gabrielsjobs@users.noreply.github.com>
2022-12-08 14:58:03 -0300

    Update Login.tsx

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Grid, Typography, TextField, Button } from '@material-ui/core';
 import { Box } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
-import useLocalStorage from 'react-use-localstorage';
 import { login } from '../../service/Service';
 import UserLogin from '../../models/UserLogin';
 import './Login.css';
+import { useDispatch } from 'react-redux';
+import { addToken } from "../../store/tokens/actions";
+import { toast } from 'react-toastify';
 
 function Login() {
     let navigate = useNavigate();
-    const [token, setToken] = useLocalStorage('token');
+    const dispatch = useDispatch();
+    const [token, setToken] = useState('');
     const [userLogin, setUserLogin] = useState<UserLogin>(
         {
             id: 0,
@@ -29,6 +32,7 @@ function Login() {
 
     useEffect(() => {
         if (token != '') {
+            dispatch(addToken(token));
             navigate('/home');
         }
     }, [token]);
@@ -37,44 +41,56 @@ function Login() {
         e.preventDefault();
         try {
             await login(`/auth/logar`, userLogin, setToken);
-
-            alert('Usuário logado com sucesso!');
+            toast.success('Usuário logado com sucesso!', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+                });
         } catch (error) {
-            alert('Dados do usuário inconsistentes. Erro ao logar!');
+            toast.error('Dados do usuário inconsistentes. Erro ao logar!', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+                });
         }
     }
 
     return (
         <Grid container className='grid1'>
             <Grid container direction='column' justifyContent='center' alignItems='center'>
-                <Box boxShadow={2} display='flex' justifyContent='center' marginTop={2} className='caixalogin'>
-                    <Typography gutterBottom color='textPrimary' align='center' className='texto1'>Entrar</Typography>
-                    <Box display='flex' justifyContent='center' >
-                        <Box marginRight={1}>
-                            <Typography variant='subtitle1' gutterBottom align='center'>Informe o seu e-mail e senha para acessar sua conta:</Typography>
+                    <Box boxShadow={2} padding={15} className='caixalogin'>
+                        <Typography variant='h4' gutterBottom color='textPrimary' component='h4' align='center' className='texto1'>Entrar</Typography>
+                        <form onSubmit={onSubmit} className='formlogin' >
+                            <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Endereço de e-mail' variant='outlined' name='usuario' margin='normal'className='teste' />
+                            <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                            <Box marginTop={5} textAlign='center'>
+                                <Button type='submit' variant='contained' className='bttlogin'>
+                                    Login
+                                </Button>
+                            </Box>
+                        </form>
+                        <Box display='flex' justifyContent='center' marginTop={2}>
+                            <Box marginRight={1}>
+                                <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                            </Box>
+                            <Link to='/cadastrousuario'>
+                                <Typography variant='subtitle1' gutterBottom align='center'>Cadastre-se</Typography>
+                            </Link>
                         </Box>
                     </Box>
-                    <form onSubmit={onSubmit} className='formlogin' >
-                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Endereço de e-mail' variant='outlined' name='usuario' margin='normal' fullWidth className='campos' />
-                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth className='campos' />
-                        <Box marginTop={5} textAlign='center'>
-                            <Button type='submit' variant='contained' className='bttlogin'>
-                                Login
-                            </Button>
-                        </Box>
-                    </form>
-                    <Box display='flex' justifyContent='center' marginTop={2}>
-                        <Box marginRight={1}>
-                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
-                        </Box>
-                        <Link to='/cadastrousuario'>
-                            <Typography variant='subtitle1' gutterBottom align='center'>Cadastre-se</Typography>
-                        </Link>
-                    </Box>
-                </Box>
+                </Grid>
             </Grid>
-        </Grid>
     );
 }
 
-export default Login
+export default Login;
